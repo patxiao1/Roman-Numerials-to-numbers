bda1055bf042064e6339a70dacd94a9202d3107e
patxiao1 <35940459+patxiao1@users.noreply.github.com>
2019-02-28 14:15:18 -0500

    update

--- ConvertRomanToNumber.java
+++ ConvertRomanToNumber.java
@@ -1,6 +1,12 @@
 public class RomanNumeral {
 	final static char symbol[] = {'M','D','C','L','X','V','I'};
 	final static int   value[] = {1000,500,100,50,10,5,1};
+	private static int[]    numbers = { 1000,  900,  500,  400,  100,   90,  
+		50,   40,   10,    9,    5,    4,    1 };
+
+	private static String[] letters = { "M",  "CM",  "D",  "CD", "C",  "XC",
+		"L",  "XL",  "X",  "IX", "V",  "IV", "I" };
+
 	
 	public static int valueOf(String roman)
 	{
@@ -14,4 +20,17 @@ public class RomanNumeral {
 		}
 		throw new IllegalArgumentException("Invalid Roman Symbol.");
 	}
+
+	public static String convertToRoman(int N)
+	{
+		String roman = "";
+		for (int i = 0; i < numbers.length; i++) {
+			while (N >= numbers[i]) {
+				roman += letters[i];
+				N -= numbers[i];
+			}
+		}
+		return roman;
+	}
+	
   }
